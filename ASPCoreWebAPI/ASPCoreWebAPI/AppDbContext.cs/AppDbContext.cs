11ac2e2251b464077d97e98e23f70ad0e679562b
Sangram-Nimap <sangram.patel@nimapinfotech.com>
2025-06-25 20:06:58 +0530

    Add files via upload

---
+++ ASPCoreWebAPI/ASPCoreWebAPI/AppDbContext.cs/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ASPCoreWebAPI.Models;
+
+namespace ASPCoreWebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ASPCoreWebAPI/ASPCoreWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASPCoreWebAPI.Models;
+using ASPCoreWebAPI.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/products
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+
+        // GET: api/products/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetById(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+                return NotFound();
+
+            return product;
+        }
+
+        // POST: api/products
+        [HttpPost]
+        public ActionResult<Product> Create(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+        // PUT: api/products/5
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product product)
+        {
+            if (id != product.Id)
+                return BadRequest();
+
+            _context.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/products/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
